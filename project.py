ae343bcbba342769c9313d784c7a4e574c51fdcc
Sreelakshmi <72209376+Sreeelakshmi@users.noreply.github.com>
2022-02-09 18:53:41 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,13 @@
+import cv2
+import dropbox
+import random
+import time
+startTime=time.time()
+def autoPrint():
+    print("xyz")
+def main():
+    while(True):
+        if((time.time()-startTime)>=1):
+            autoPrint()
+            
+main()
